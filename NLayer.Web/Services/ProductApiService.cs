77c1e099d82c00c64f6830d6fd90fb7cdb59b400
Kadiiravsaar <kadiiravsar@gmail.com>
2023-08-01 16:39:16 +0300

    ProductApiService => GetProductWithCategoryDto & Save Added

--- NLayer.Web/Services/ProductApiService.cs
+++ NLayer.Web/Services/ProductApiService.cs
@@ -1,4 +1,6 @@
-﻿namespace NLayer.Web.Services
+﻿using NLayer.Core.DTOs;
+
+namespace NLayer.Web.Services
 {
     public class ProductApiService
     {
@@ -7,6 +9,31 @@
         public ProductApiService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        public async Task<List<ProductWithCategoryDto>> GetProductWithCategoryDto()
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<List<ProductWithCategoryDto>>>("ProdWithCategory");
+            return response.Data;
+
+            // GetFromJsonAsync direk datayı json olarak almamızı sağlar
+
+            // nasıl bir data bekliyorum sorusunun cevabı
+            // <Apiden o controllerda nasıl beklediğine bağlı (<CustomResponseDto<List<ProductWithCategoryDto>>>)> bu beklemiş olduğun tiptir
+
+            // nereye istek yapmak istiyorsun bunu veriyor <>(Burada) ("ProdWithCategory")
+        }
+
+
+        public async Task<ProductDto> Save(ProductDto newProduct)
+        {
+            var response = await _httpClient.PostAsJsonAsync("addProduct", newProduct);
+
+            if (!response.IsSuccessStatusCode) return null; // burayı geçerse tamam demektir
+
+            var responseBody = await response.Content.ReadFromJsonAsync<CustomResponseDto<ProductDto>>();
+
+            return responseBody.Data;
         }
     }
 }
